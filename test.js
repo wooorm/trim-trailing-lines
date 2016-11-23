d27816972f8402fc9ea7b3f7d53f29c74d4ac515
Titus Wormer <tituswormer@gmail.com>
2016-11-23 20:50:39 +0100

    Refactor code-style

--- test.js
+++ test.js
@@ -2,27 +2,19 @@
 
 /* eslint-env mocha */
 
-/*
- * Dependencies.
- */
-
-var trimTrailingLines = require('./');
 var assert = require('assert');
-
-/*
- * Tests.
- */
+var trimTrailingLines = require('./');
 
 describe('trimTrailingLines(value)', function () {
-    it('should coerce to string', function () {
-        assert.equal(trimTrailingLines(true), 'true');
-    });
+  it('should coerce to string', function () {
+    assert.equal(trimTrailingLines(true), 'true');
+  });
 
-    it('should work', function () {
-        assert.equal(trimTrailingLines(''), '');
-        assert.equal(trimTrailingLines('foo'), 'foo');
-        assert.equal(trimTrailingLines('foo\nbar'), 'foo\nbar');
-        assert.equal(trimTrailingLines('foo\nbar\n'), 'foo\nbar');
-        assert.equal(trimTrailingLines('foo\nbar\n\n'), 'foo\nbar');
-    });
+  it('should work', function () {
+    assert.equal(trimTrailingLines(''), '');
+    assert.equal(trimTrailingLines('foo'), 'foo');
+    assert.equal(trimTrailingLines('foo\nbar'), 'foo\nbar');
+    assert.equal(trimTrailingLines('foo\nbar\n'), 'foo\nbar');
+    assert.equal(trimTrailingLines('foo\nbar\n\n'), 'foo\nbar');
+  });
 });
