d8bee2f5fb9da3c10334792f9727bb39fc2db120
Titus Wormer <tituswormer@gmail.com>
2018-04-28 12:12:06 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape');
-var trimTrailingLines = require('./');
+var trimTrailingLines = require('.');
 
 test('trimTrailingLines(value)', function (t) {
   t.equal(trimTrailingLines(true), 'true', 'should coerce to string');
