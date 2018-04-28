496461283ac499cca98117241ebd0681fd27429c
Titus Wormer <tituswormer@gmail.com>
2018-04-28 12:13:58 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var trimTrailingLines = require('.');
+var test = require('tape')
+var trimTrailingLines = require('.')
 
-test('trimTrailingLines(value)', function (t) {
-  t.equal(trimTrailingLines(true), 'true', 'should coerce to string');
-  t.equal(trimTrailingLines(''), '', 'should work (1)');
-  t.equal(trimTrailingLines('foo'), 'foo', 'should work (2)');
-  t.equal(trimTrailingLines('foo\nbar'), 'foo\nbar', 'should work (3)');
-  t.equal(trimTrailingLines('foo\nbar\n'), 'foo\nbar', 'should work (4)');
-  t.equal(trimTrailingLines('foo\nbar\n\n'), 'foo\nbar', 'should work (5)');
-  t.end();
-});
+test('trimTrailingLines(value)', function(t) {
+  t.equal(trimTrailingLines(true), 'true', 'should coerce to string')
+  t.equal(trimTrailingLines(''), '', 'should work (1)')
+  t.equal(trimTrailingLines('foo'), 'foo', 'should work (2)')
+  t.equal(trimTrailingLines('foo\nbar'), 'foo\nbar', 'should work (3)')
+  t.equal(trimTrailingLines('foo\nbar\n'), 'foo\nbar', 'should work (4)')
+  t.equal(trimTrailingLines('foo\nbar\n\n'), 'foo\nbar', 'should work (5)')
+  t.end()
+})
