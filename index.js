b6c2331fc4d501c1cb38dafd7796260970e5bc0b
Titus Wormer <tituswormer@gmail.com>
2022-11-14 10:27:10 +0100

    Add support for CR, CRLF

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 /**
  * Remove final newline characters from `value`.
  *
- * @param {unknown} value Value with trailing line feeds, coerced to string.
- * @return {string} Value without trailing newlines.
+ * @param {unknown} value
+ *   Value with trailing line feeds, coerced to string.
+ * @return {string}
+ *   Value without trailing newlines.
  */
 export function trimTrailingLines(value) {
-  return String(value).replace(/\n+$/, '')
+  const input = String(value)
+  let end = input.length
+
+  while (end > 0) {
+    const code = input.codePointAt(end - 1)
+    if (code !== undefined && (code === 10 || code === 13)) {
+      end--
+    } else {
+      break
+    }
+  }
+
+  return input.slice(0, end)
 }
--- test.js
+++ test.js
@@ -9,4 +9,9 @@ test('trimTrailingLines(value)', function () {
   assert.equal(trimTrailingLines('foo\nbar'), 'foo\nbar', 'should work (3)')
   assert.equal(trimTrailingLines('foo\nbar\n'), 'foo\nbar', 'should work (4)')
   assert.equal(trimTrailingLines('foo\nbar\n\n'), 'foo\nbar', 'should work (5)')
+
+  assert.equal(trimTrailingLines('a\n'), 'a', 'lf')
+  assert.equal(trimTrailingLines('a\r'), 'a', 'cr')
+  assert.equal(trimTrailingLines('a\r\n'), 'a', 'cr+lf')
+  assert.equal(trimTrailingLines('\r\r\n\n'), '', 'eols only')
 })
