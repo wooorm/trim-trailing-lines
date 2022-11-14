8a42ce86e99aeded27e3c91879d9674ce1687ad4
Titus Wormer <tituswormer@gmail.com>
2022-11-14 10:24:06 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {trimTrailingLines} from './index.js'
 
-test('trimTrailingLines(value)', function (t) {
-  t.equal(trimTrailingLines(true), 'true', 'should coerce to string')
-  t.equal(trimTrailingLines(''), '', 'should work (1)')
-  t.equal(trimTrailingLines('foo'), 'foo', 'should work (2)')
-  t.equal(trimTrailingLines('foo\nbar'), 'foo\nbar', 'should work (3)')
-  t.equal(trimTrailingLines('foo\nbar\n'), 'foo\nbar', 'should work (4)')
-  t.equal(trimTrailingLines('foo\nbar\n\n'), 'foo\nbar', 'should work (5)')
-  t.end()
+test('trimTrailingLines(value)', function () {
+  assert.equal(trimTrailingLines(true), 'true', 'should coerce to string')
+  assert.equal(trimTrailingLines(''), '', 'should work (1)')
+  assert.equal(trimTrailingLines('foo'), 'foo', 'should work (2)')
+  assert.equal(trimTrailingLines('foo\nbar'), 'foo\nbar', 'should work (3)')
+  assert.equal(trimTrailingLines('foo\nbar\n'), 'foo\nbar', 'should work (4)')
+  assert.equal(trimTrailingLines('foo\nbar\n\n'), 'foo\nbar', 'should work (5)')
 })
